d6073c4b3ba3d2af81d70d6363685e2ba0b6f10b
GonzaloRP11 <gonzalopelizza@hotmail.com>
2025-09-05 00:44:41 -0300

    Se arregla función y configuración de terminal

--- src/main.py
+++ src/main.py
@@ -3,60 +3,54 @@ import textwrap as textwrap
 
 def anchoLargoTerminal():
     lineas = sistema.get_terminal_size().lines
-    columnas = sistema.get_terminal_size().columns if (sistema.get_terminal_size().columns >= 100) else 80
-    bordeIzquierdo = (columnas//3)
-    centro = (columnas//3) + (columnas//3)
-    bordeDerecho = (columnas//3)
+    if sistema.get_terminal_size().columns < 80:
+        print("Su terminal es demasiado pequeña para jugar.")
+        return
+    else:
+        columnas = sistema.get_terminal_size().columns if (sistema.get_terminal_size().columns >= 100) else 80
+
+    bordeIzquierdo = (columnas//4)
+    centro = bordeIzquierdo * 2
+    bordeDerecho = bordeIzquierdo
 
     return lineas,columnas,bordeIzquierdo,centro,bordeDerecho
 
-def imprimirSeparador(li,col,bz,c,bd):
-    bordeIzquierdo = (col//3)
-    centro = (col//4)
-    bordeDerecho = (col//3)
-    
+def imprimirSeparador(lineas,columnas,bordeIzquierdo,centro,bordeDerecho):
 
-    listaAsteriscos = ['*' for n in range(col)]
-    # parrafo = textwrap.wrap(listaAsteriscos,centro)
-    """print(parrafo)"""
-    """print(listaAsteriscos.ljust(bordeIzquierdo))"""
+    listaAsteriscos = '*' * centro
+    for a in range(2):
+        print(listaAsteriscos.center(centro + bordeIzquierdo))
+   
     
  
-
-def imprimirCabecera():
-    lineas,columnas,bordeIzquierdo,centro,bordeDerecho = anchoLargoTerminal()  
+#Impresión de cabecera principal 
+def imprimirCabecera(lineas,columnas,bordeIzquierdo,centro,bordeDerecho):
     imprimirSeparador(lineas,columnas,bordeIzquierdo,centro,bordeDerecho)
+    imprimirBienvenida(lineas,columnas,bordeIzquierdo,centro,bordeDerecho)
 
-def imprimirBienvenida():
-    lineas,columnas,bordeIzquierdo,centro,bordeDerecho = anchoLargoTerminal()
-
-    complemento = "*"
+# Impresión del título de bienvenida al jugador
+def imprimirBienvenida(lineas,columnas,bordeIzquierdo,centro,bordeDerecho ):
+    complemento = " "
     mensajeBienvenida = "¡Bienvenido a Destiny Weaver!"
     print(
-            complemento.ljust(bordeIzquierdo) + 
-            mensajeBienvenida.center(0) + 
-            complemento.rjust(bordeDerecho) 
+            mensajeBienvenida.center(centro + bordeIzquierdo)
         )
     print("\n")
     
     mensajeDescripcion = "Has llegado al borde de un mundo inexplorado, un lugar forjado por historias y el poder de las elecciones. Al dar tu primer paso, te sumerges en un tejido de destinos que se irá formando con cada una de tus decisiones.\n En este viaje, cada hilo que unes te conecta a un destino único. El futuro no está escrito; tú eres el tejedor de tu propia historia."
     
-    parrafo = textwrap.wrap(mensajeDescripcion,(columnas//2))
-    complemento = " "
-    for linea in parrafo:
+    parrafo = textwrap.fill(mensajeDescripcion,centro)
 
-        print(
-                complemento.ljust((bordeIzquierdo - centro))+
-                linea.center(centro)+
-                complemento.rjust((bordeDerecho ))
-             )
-    
-    
+    for oracion in parrafo.splitlines():
+        print( " ".ljust(bordeIzquierdo // 2) +
+                oracion +
+               " ".rjust(bordeDerecho)
+            )
 
-     
 
+#Comienzo del programa
 def main ():
-    imprimirCabecera()
-    imprimirBienvenida()
+    lineas,columnas,bordeIzquierdo,centro,bordeDerecho = anchoLargoTerminal()
+    imprimirCabecera(lineas,columnas,bordeIzquierdo,centro,bordeDerecho)
 
 main()
